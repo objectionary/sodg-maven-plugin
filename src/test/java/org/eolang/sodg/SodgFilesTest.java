344129b60e573dd928e31c73098adfd50ff37e39
h1alexbel <h1alexbelx@gmail.com>
2025-10-03 21:57:28 +0300

    bug(#103): add new test classes SodgDefaultConfig and SodgFilesTest for sodg module

---
+++ src/test/java/org/eolang/sodg/SodgFilesTest.java
@@ -0,0 +1,15 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2016-2025 Objectionary.com
+ * SPDX-License-Identifier: MIT
+ */
+package org.eolang.sodg;
+
+/**
+ * Tests for {@link SodgFiles}.
+ *
+ * @since 0.0.3
+ */
+final class SodgFilesTest {
+
+
+}
