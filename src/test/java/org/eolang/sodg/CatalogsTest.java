2681767139493b3a8faccb66836f8f78a71f873e
h1alexbel <h1alexbelx@gmail.com>
2025-10-06 13:09:33 +0300

    bug(#114): add javax.json dependencies and update CatalogsTest to use MnJson for foreigns data

--- src/test/java/org/eolang/sodg/CatalogsTest.java
+++ src/test/java/org/eolang/sodg/CatalogsTest.java
@@ -6,10 +6,11 @@ package org.eolang.sodg;
 
 import com.yegor256.Mktmp;
 import com.yegor256.MktmpResolver;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
+import com.yegor256.tojos.MnJson;
 import java.nio.file.Path;
+import org.cactoos.list.ListOf;
+import org.cactoos.map.MapEntry;
+import org.cactoos.map.MapOf;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
@@ -18,22 +19,30 @@ import org.junit.jupiter.api.extension.ExtendWith;
 /**
  * Tests for {@link Catalogs}.
  *
- * @since 0.0.3
+ * @since 0.0.4
  */
 final class CatalogsTest {
 
     @Test
     @ExtendWith(MktmpResolver.class)
-    void makesInJson(@Mktmp final Path temp) throws IOException {
+    void makesInJson(@Mktmp final Path temp) {
+        final Path foreigns = temp.resolve("foreigns.json");
+        new MnJson(foreigns).write(
+            new ListOf<>(
+                new MapOf<>(
+                    new MapEntry<>("id", "org.eolang.sodg.examples.应用程序")
+                ),
+                new MapOf<>(
+                    new MapEntry<>("id", "org.eolang.io.stdout")
+                ),
+                new MapOf<>(
+                    new MapEntry<>("id", "org.eolang.io.系统故障")
+                )
+            )
+        );
         MatcherAssert.assertThat(
             "Size of the returned tojos does not match with expected",
-            Catalogs.INSTANCE.make(
-                Files.write(
-                    temp.resolve("foreigns.json"),
-                    "[1, 2, 3]".getBytes(StandardCharsets.UTF_8)
-                ),
-                "json"
-            ).select(t -> true).size(),
+            Catalogs.INSTANCE.make(foreigns, "json").select(t -> true).size(),
             Matchers.equalTo(3)
         );
     }
